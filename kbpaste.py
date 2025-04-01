fdc9cd40d3f2cd830b9d073473ab120fdd5bd2b3
Remi Rampin <remi@rampin.org>
2025-04-01 14:49:55 -0400

    Add more delay

--- kbpaste.py
+++ kbpaste.py
@@ -6,9 +6,28 @@ from time import sleep
 DELAY = 3
 
 
+def write(message, interval=0.0):
+    last_upper = False
+    for c in message:
+        upper = c.isupper()
+        c = c.lower()
+        if upper and not last_upper:
+            pyautogui.keyDown('shift')
+            sleep(interval)
+        elif not upper and last_upper:
+            pyautogui.keyUp('shift')
+            sleep(interval)
+        last_upper = upper
+
+        pyautogui.keyDown(c)
+        sleep(interval)
+        pyautogui.keyUp(c)
+        sleep(interval)
+
+
 def main():
     text = pyperclip.paste()
     print("Waiting {0} seconds and pasting {1} characters...".format(DELAY, len(text)))
     sleep(DELAY)
-    pyautogui.write(text)
+    write(text, interval=0.1)
     print("Done.")
